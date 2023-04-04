b1c8d2d25242532e62baee6071f810544acd28a0
fadi <fadi.m.marouf@gmail.com>
2023-04-04 14:49:37 +0200

    add about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const about = () => {
-    return <div>about</div>;
+    return (
+        <div>
+            <h1>About us Page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+                sint. Harum laborum expedita beatae doloribus, provident animi
+                architecto. Ea omnis possimus repellendus beatae eum ullam,
+                voluptatem molestias distinctio architecto praesentium.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit,
+                sint. Harum laborum expedita beatae doloribus, provident animi
+                architecto. Ea omnis possimus repellendus beatae eum ullam,
+                voluptatem molestias distinctio architecto praesentium.
+            </p>
+        </div>
+    );
 };
 
 export default about;
