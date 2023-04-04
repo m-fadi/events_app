3f22db71a408bee519948a985d5a391159472529
fadi <fadi.m.marouf@gmail.com>
2023-04-04 14:49:58 +0200

    add events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function events() {
-    return <div>events</div>;
+    return (
+        <div>
+            <h1> Events Page </h1>
+            
+                <h2> Events in London</h2>
+                
+                <a href="http://">
+                    <img />
+                </a>
+                <h2> Events in Sanfransicco</h2>
+              
+                <a href="http://">
+                    <img />
+                </a>
+                <h2> Events in Barcelona</h2>
+               
+                <a href="http://">
+                    <img />
+                </a>
+            
+        </div>
+    );
 }
 
 export default events;
